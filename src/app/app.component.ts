e7c11ae13ad32fb2490c0bcace491c2b94474715
Your Name <macielbrayn@gmail.com>
2026-02-10 17:10:19 -0600

    Update de codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from "./header/header";
 
@@ -9,4 +10,19 @@ import { HeaderComponent } from "./header/header";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  tituloModulo = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.url.includes('dashboard')) this.tituloModulo = 'Dashboard Administrativo';
+        else if (event.url.includes('actividades')) this.tituloModulo = 'Actividades';
+        else if (event.url.includes('asistencia')) this.tituloModulo = 'Asistencia';
+        else if (event.url.includes('reportes')) this.tituloModulo = 'Reportes';
+        else if (event.url.includes('cuenta')) this.tituloModulo = 'Cuenta';
+        else this.tituloModulo = '';
+      }
+    });
+  }
+}
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,4 +8,6 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule]
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input() tituloModulo: string = ''; 
+}
